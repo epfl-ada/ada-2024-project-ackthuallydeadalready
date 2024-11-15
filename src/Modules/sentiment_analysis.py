5e26823ee19bfc3b45db07114def0dc6ae2b65f9
Alexandre Martel <alexandre.martel@epfl.ch>
2024-11-15 19:42:26 +0100

    added comments to sentiment_analysis.py and removed unnecessary line (tokenizer) and package (autotokenizer, not needed yet)

--- src/Modules/sentiment_analysis.py
+++ src/Modules/sentiment_analysis.py
@@ -1,16 +1,18 @@
 import src.Modules.plotting
-from transformers import AutoTokenizer, pipeline
-#from transformers import AutoModelForSequenceClassification #to use a longformer, not needed yet
+from transformers import pipeline
+#from transformers import AutoModelForSequenceClassification, AutoTokenizer # Respectively to use a longformer and totokenize,
+# not needed yet
 import torch
 
+# Randomly samples 10% of rows in a dataframe provided, is used to conduct the preliminary sentiment analysis faster
 def sampling(df, prop=0.1):
     return df.sample(frac=prop)
 
-def sa_hug(df,max=512):
-    device = 0 if torch.cuda.is_available() else -1
-    tokenizer = AutoTokenizer.from_pretrained('bert-base-uncased')
-    sentiment_analyzer = pipeline("sentiment-analysis", truncation=True, max_length = max, device=device)
-    results = sentiment_analyzer(df['TXT'].tolist()) #512 with shorter model did not work
+# FUnction that performs a sentiment analysis using a hugginface model 
+def sa_hug(df,max=512): # Allows user to specify the max number of tokens
+    device = 0 if torch.cuda.is_available() else -1 # Uses a gpu if available
+    sentiment_analyzer = pipeline("sentiment-analysis", truncation=True, max_length = max, device=device) # Performs the senitment analysis
+    results = sentiment_analyzer(df['TXT'].tolist())
     df['sentiment'] = [result['label'] for result in results]
     df['confidence'] = [result['score'] for result in results]
     return df
